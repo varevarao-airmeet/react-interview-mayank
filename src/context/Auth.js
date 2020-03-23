cb0a6dcf9dcf9e794b7d72ad43f44d15407d525b
Varshneya Rao <59429662+varevarao-airmeet@users.noreply.github.com>
2020-03-23 13:17:33 +0530

    Update Auth.js

--- src/context/Auth.js
+++ src/context/Auth.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+export const CONNECTED_STATUS = {
+    CONNECTED: 'Connected',
+    NOT_CONNECTED: 'Disconnected'
+
 const DEFAULT_USER = {
     name: 'Mayank',
     company: 'Auriga',
@@ -9,7 +13,7 @@ const DEFAULT_USER = {
 
 const Auth = React.createContext({
     user: DEFAULT_USER,
-    status: 'Connected'
+    status: CONNECTED_STATUS.NOT_CONNECTED
 });
 
 export default Auth;
