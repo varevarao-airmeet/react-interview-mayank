5e56cf91304ed0b04cba0174866cb65e09ca678e
Mayank Bhootra <mayankbhootra.official@gmail.com>
2020-03-23 16:37:28 +0530

    using context and hooks

--- src/context/Auth.js
+++ src/context/Auth.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export const CONNECTED_STATUS = {
     CONNECTED: 'Connected',
     NOT_CONNECTED: 'Disconnected'
+};
 
 const DEFAULT_USER = {
     name: 'Mayank',
     company: 'Auriga',
     location: 'Udaipur',
     project: 'Airmeet'
-}
+};
 
 const Auth = React.createContext({
     user: DEFAULT_USER,
--- src/hooks/useFakeConnection.js
+++ src/hooks/useFakeConnection.js
@@ -1,11 +1,22 @@
+import React from 'React';
+import Auth, { CONNECTED_STATUS } from '../context/Auth';
+import { connectFake } from '../utils/connect';
 /**
  * This function should
  * 1. use the current user under the `Auth` context,
  * 2. Only the first time: call the `connectFake` function under `src/utils/connect.js` using this user
  * 3. Return 'NOT-CONNECTED', or 'CONNECTED' depending on the connection result
  */
-function useFakeConnection() {
-    
+async function useFakeConnection() {
+  const currentStatus = React.useContext(Auth);
+  const fetchStatus = await connectFake(currentStatus.user.name);
+
+  if (fetchStatus) {
+    currentStatus.setCurrentStatus(CONNECTED_STATUS.CONNECTED);
+  } else {
+    currentStatus.setCurrentStatus(CONNECTED_STATUS.NOT_CONNECTED);
+  }
+  // return currentStatus.status.toUpperCase();
 }
 
 export default useFakeConnection
